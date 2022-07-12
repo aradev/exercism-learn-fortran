8a87e20aef6c3f160de215c27fa91b3d8d91cf8e
aradev <aradev@users.noreply.github.com>
2022-07-12 21:28:19 +0100

    Triangles final

--- triangle/triangle.f90
+++ triangle/triangle.f90
@@ -27,12 +27,71 @@ module triangle
  logical function is_triangle_real(edges)
     real,dimension(3) :: edges
 
+    if ( .not. edges(1) > 0 ) then
+      is_triangle_real = .false.
+      return
+    end if
+
+    if ( .not. edges(2) > 0 ) then
+      is_triangle_real = .false.
+      return
+    end if
+
+    if ( .not. edges(3) > 0 ) then
+      is_triangle_real = .false.
+      return
+    end if
+
+    if ( edges(1) + edges(2) < edges(3) ) then
+      is_triangle_real = .false.
+      return
+    end if
+
+    if ( edges(3) + edges(2) < edges(1) ) then
+      is_triangle_real = .false.
+      return
+    end if
+
+    if ( edges(3) + edges(1) < edges(2) ) then
+      is_triangle_real = .false.
+      return
+    end if
+
     is_triangle_real = .true.
   end function
 
   logical function is_triangle_int(edges)
     integer, dimension(3) :: edges
 
+    if ( .not. edges(1) > 0 ) then
+      is_triangle_int = .false.
+      return
+    end if
+
+    if ( .not. edges(2) > 0 ) then
+      is_triangle_int = .false.
+      return
+    end if
+
+    if ( .not. edges(3) > 0 ) then
+      is_triangle_int = .false.
+      return
+    end if
+
+    if ( edges(1) + edges(2) < edges(3) ) then
+      is_triangle_int = .false.
+      return
+    end if
+
+    if ( edges(3) + edges(2) < edges(1) ) then
+      is_triangle_int = .false.
+      return
+    end if
+
+    if ( edges(3) + edges(1) < edges(2) ) then
+      is_triangle_int = .false.
+      return
+    end if
 
     is_triangle_int = .true.
   end function
@@ -40,6 +99,11 @@ module triangle
 
   logical function equilateral_real(edges)
     real,dimension(3) :: edges
+
+    if ( .not. is_triangle(edges) ) then
+      equilateral_real = .false.
+      return
+    end if
 
     if ( (abs(edges(1) - edges(2)) > 1.0D-6)) then
       equilateral_real = .false.
@@ -62,6 +126,10 @@ module triangle
   logical function equilateral_int(edges)
     integer,dimension(3) :: edges
 
+    if ( .not. is_triangle(edges) ) then
+      equilateral_int = .false.
+      return
+    end if
 
     if ( (abs(edges(1) - edges(2)) > 1.0D-6)) then
       equilateral_int = .false.
@@ -83,23 +151,86 @@ module triangle
 
   logical function isosceles_real(edges)
     real,dimension(3) :: edges
+
+    if ( .not. is_triangle(edges) ) then
+      isosceles_real = .false.
+      return
+    end if
+
+    if ( (abs(edges(1) - edges(2)) < 1.0D-6)) then
+      isosceles_real = .true.
+      return
+    end if
+
+    if ( (abs(edges(1) - edges(3)) < 1.0D-6)) then
+      isosceles_real = .true.
+      return
+    end if
+
+    if ( (abs(edges(2) - edges(3)) < 1.0D-6)) then
+      isosceles_real = .true.
+      return
+    end if
+
     isosceles_real = .false.
   end function
 
   logical function isosceles_int(edges)
     integer,dimension(3) :: edges
+
+    if ( .not. is_triangle(edges) ) then
+      isosceles_int = .false.
+      return
+    end if
+
+    if ( (abs(edges(1) - edges(2)) < 1.0D-6)) then
+      isosceles_int = .true.
+      return
+    end if
+
+    if ( (abs(edges(1) - edges(3)) < 1.0D-6)) then
+      isosceles_int = .true.
+      return
+    end if
+
+    if ( (abs(edges(2) - edges(3)) < 1.0D-6)) then
+      isosceles_int = .true.
+      return
+    end if
+
     isosceles_int = .false.
   end function
 
-
   logical function scalene_real(edges)
     real,dimension(3) :: edges
-    scalene_real = .false.
+
+    if ( .not. is_triangle(edges) ) then
+      scalene_real = .false.
+      return
+    end if
+
+    if (isosceles(edges) ) then
+      scalene_real = .false.
+      return
+    end if
+
+    scalene_real = .true.
   end function
 
   logical function scalene_int(edges)
     integer,dimension(3) :: edges
-    scalene_int = .false.
+
+    if ( .not. is_triangle(edges) ) then
+      scalene_int = .false.
+      return
+    end if
+
+    if (isosceles(edges) ) then
+      scalene_int = .false.
+      return
+    end if
+
+    scalene_int = .true.
   end function
 
 end module
